04b5335c2bc902f336db7cf0432f9060d4047e8f
lokesh-mt <lokesh.mt@gmail.com>
2017-06-15 01:26:04 -0400

    Create configure-remote-state.sh

--- docker-demo-3/scripts/configure-remote-state.sh
+++ docker-demo-3/scripts/configure-remote-state.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-terraform init -backend=s3 -backend-config="bucket=terraform-state-aet935" -backend-config="key=terraform/terraform.tfstate" -backend-config="region=us-east-1"
+terraform init -backend-config="bucket=terraform-state-aet935" -backend-config="key=terraform/terraform.tfstate" -backend-config="region=us-east-1"
